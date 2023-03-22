551c0b61cfc6c5ee8348cada4ac9dd11114c4da6
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-03-22 22:30:32 +0100

    add tack 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,23 @@
-const actions = document.querySelectorAll(`#counter button`);
-console.log(actions);
+const decrementBtn = document.querySelector(
+  `button[data-action = "decrement"]`
+);
+const valueEl = document.querySelector(`#value`);
+
+const incrementBtn = document.querySelector(
+  `button[data-action = "increment"]`
+);
+
+let counterValue = 0;
+
+decrementBtn.addEventListener(`click`, onDecrementBtnClick);
+function onDecrementBtnClick() {
+  valueEl.textContent = counterValue;
+  counterValue -= 1;
+  console.log(counterValue);
+}
+incrementBtn.addEventListener(`click`, onIncrementBtnClick);
+function onIncrementBtnClick() {
+  valueEl.textContent = counterValue;
+  counterValue += 1;
+  console.log(counterValue);
+}
