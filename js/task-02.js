fe3e6ea548093f419e25fc3eaa74e9607d62d44f
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-03-26 18:05:53 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,8 +7,18 @@ const ingredients = [
   "Condiments",
 ];
 
+// const listEl = document.querySelector("#ingredients");
+// const listItem = ingredients
+//   .map((item) => `<li class="item">${item}</li>`)
+//   .join("");
+// listEl.insertAdjacentHTML("beforeend", listItem);
+
 const listEl = document.querySelector("#ingredients");
-const listItem = ingredients
-  .map((item) => `<li class="item">${item}</li>`)
-  .join("");
-listEl.insertAdjacentHTML("beforeend", listItem);
+const listItem = ingredients.map((ingredient) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = ingredient;
+  return listItemEl;
+});
+
+listEl.append(...listItem);
+console.log(listEl);
